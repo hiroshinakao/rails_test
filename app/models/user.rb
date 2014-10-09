fa99dbbd0bc2ab7684046c18df02d73572ae3266
hiroshinakao <hiroshi.nakao@gmail.com>
2014-10-09 12:46:36 +0900

    ユーザーとイイねの関連を定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  has_many :likes, dependent: :destroy
 end
