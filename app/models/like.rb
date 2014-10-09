a00f9674235ebc462ebb6d4b4886aa6423756dfd
hiroshinakao <hiroshi.nakao@gmail.com>
2014-10-09 12:31:12 +0900

    $ rake db:migrate

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id            :integer          not null, primary key
+#  user_id       :integer
+#  likeable_id   :integer
+#  likeable_type :string(255)
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+# Indexes
+#
+#  index_likes_on_likeable_id_and_likeable_type  (likeable_id,likeable_type)
+#  index_likes_on_user_id                        (user_id)
+#
+
 class Like < ActiveRecord::Base
   belongs_to :user
   belongs_to :likeable, polymorphic: true
