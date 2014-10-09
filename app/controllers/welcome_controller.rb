a24fca8b156fb827ee43df10b59b022b29117f52
hiroshinakao <hiroshi.nakao@gmail.com>
2014-10-09 17:07:48 +0900

    Arel版を画面に追加

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,5 +11,11 @@ class WelcomeController < ApplicationController
     @clazz_top = Clazz.rank_of_like_count.first
     @student_top = Student.rank_of_like_count.first
     @examination_top = Examination.rank_of_like_count.first
+
+    #イイね数ランキング(Arel)
+    @grade_top_arel = Grade.rank_of_like_count_arel.first
+    @clazz_top_arel = Clazz.rank_of_like_count_arel.first
+    @student_top_arel = Student.rank_of_like_count_arel.first
+    @examination_top_arel = Examination.rank_of_like_count_arel.first
   end
 end
