d5af121bdd23c1fe1938aa11c04d4aeb4ed3d7a8
hiroshinakao <hiroshi.nakao@gmail.com>
2014-10-09 15:14:48 +0900

    クラス別の最高得点者も画面表示

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@ class WelcomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    # クラスごとの最高得点者
+    @excellent_students = Student.top_score_in_each_clazz
+
     #イイね数ランキング
     @grade_top = Grade.rank_of_like_count.first
     @clazz_top = Clazz.rank_of_like_count.first
